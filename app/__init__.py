0d3787d39a58c32ade1152586605302f1a81060a
Murphian Xiao <murphianx@gmail.com>
2019-04-06 12:31:02 +0800

    Add models and forms.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['FLASKY_ADMIN'] = 'xiao0yu0xiang0@qq.com' #!! from $env
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    return render_template('login.html')
+
