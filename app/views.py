26916a32e19d62fc17369fb28fd82fa26cf2734c
Murphian Xiao <murphianx@gmail.com>
2019-04-07 12:30:03 +0800

    Solve flash delay bug.(not work in index.html)

--- app/views.py
+++ app/views.py
@@ -6,8 +6,13 @@ from app.forms import RegistrationForm, LoginForm
 
 @app.route('/')
 def index():
-    logout_user() #!!
     return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+    logout_user() 
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
